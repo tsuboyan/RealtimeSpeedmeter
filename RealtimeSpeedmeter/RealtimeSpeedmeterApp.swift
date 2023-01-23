b265a1f954ac62f0f48d050f017260633778d5b5
atsushi.otsubo <m@rirex.net>
2023-01-23 22:44:22 +0900

    initial commit

---
+++ RealtimeSpeedmeter/RealtimeSpeedmeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeSpeedmeterApp.swift
+//  RealtimeSpeedmeter
+//
+//  Created by Atsushi Otsubo on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeSpeedmeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
