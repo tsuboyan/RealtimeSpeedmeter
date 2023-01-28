6baafbb7ff760ea3480682b97904f9623e2d5dbc
atsushi.otsubo <m@rirex.net>
2023-01-28 18:14:50 +0900

    Implement speedmeter

--- RealtimeSpeedmeter/SpeedCalculator.swift
+++ RealtimeSpeedmeter/SpeedCalculator.swift
@@ -8,7 +8,48 @@
 import Foundation
 
 struct SpeedCalculator {
-    static func calculateSpeed(currentSpeed: Double, acceleration: Double, delta: Double) -> Double {
-        currentSpeed + (acceleration * delta)
+    static func calculateSpeed(currentSpeed: Double, acc: Double, delta: Double, accThresh: Double) -> Double {
+        // ドリフト防止のため、加速度が閾値より大きい間だけ速度を更新する
+        abs(acc) > accThresh ? (currentSpeed + (acc * delta)) : currentSpeed
+    }
+    
+    /// 3軸成分から水平1軸成分のみを取り出す
+    static func calculateHorizontalComponent(x: Double, y: Double, z: Double, roll rollRow: Double, pitch pitchRow: Double) -> Double {
+        // 単位を度数に変換
+        let rollDeg = rollRow * 180.0 / Double.pi
+        let pitchDeg = pitchRow * 180.0 / Double.pi
+        // -90度から+90度の範囲となるよう正規化
+        let roll90: Double
+        if (rollDeg > 90.0) { // 傾きが90度を上回っている場合, 超えている分を90から引く
+            roll90 = 90.0 - (rollDeg - 90.0)
+        } else if (rollDeg < -90) {
+            roll90 = -90.0 - (rollDeg + 90.0)
+        } else {
+            roll90 = rollDeg
+        }
+        let pitch90 = pitchDeg // pitchは元から-90から+90の範囲になっているのでそのまま
+        
+        let rollRadian = roll90 / (180.0 / Double.pi)
+        let pitchRadian = pitch90 / (180.0 / Double.pi)
+        
+        let xWeight = cos(rollRadian)
+        let yWeight = cos(pitchRadian)
+        let zWeight = abs(rollRadian) > abs(pitchRadian) ? abs(sin(rollRadian)) : abs(sin(pitchRadian))
+        
+        let xHorizontal = x * xWeight
+        let yHorizontal = y * yWeight
+        let zHorizontal = z * zWeight
+        
+        // 三平方の定理で水平方向成分を合成する
+        let horizontalComponent = sqrt(pow(xHorizontal, 2) +
+                                       pow(yHorizontal, 2) +
+                                       pow(zHorizontal, 2))
+        // 方向を考慮する (z軸方向で考慮)
+        return horizontalComponent * (z > 0 ? 1 : -1)
+    }
+    
+    // 平滑化フィルタ
+    static func smooth(current: Double, previous: Double) -> Double {
+        current * 0.2 + previous * 0.8
     }
 }
--- RealtimeSpeedmeter/SpeedmeterPresenter.swift
+++ RealtimeSpeedmeter/SpeedmeterPresenter.swift
@@ -9,32 +9,59 @@ import Combine
 import Foundation
 
 @MainActor final class SpeedmeterViewState: ObservableObject {
-    @Published var accelerationY: Double = 0
-    @Published var speedY: Double = 0
+    @Published var acc: Double = 0
+    @Published var speed: Double = 0
     
-    var speedYKM: Double {
-        (speedY / 1000) * 60
+    var speedKiloMeter: Double {
+        abs(speed * 60)
+    }
+    
+    var stopping: Bool {
+        acc < 0.005
     }
 }
 
 @MainActor final class SpeedmeterPresenter {
-    let delta: Double = 0.1
+    let fps: Double = 30
+    let accThresh: Double = 0.01 // この加速度以上であれば速度を計算する (ドリフト防止)
+    let stoppingThresh: Double = 0.005 // この加速度以下の状態が stoppingResetInterval 秒間続くと速度をリセットする (ドリフト防止)
+    let stoppingResetInterval: Double = 5.0
     let sensor: AccelerationSensor
     let state: SpeedmeterViewState
+    private var smoothedHorizontalAcc: Double = 0
+    private var stoppingCounter: Int = 0
     private var cancellables: Set<AnyCancellable> = []
     
     init(state: SpeedmeterViewState) {
-        sensor = AccelerationSensor(delta: delta)
+        sensor = AccelerationSensor(delta: 1 / fps)
         self.state = state
     }
     
     func onAppear() {
         sensor.updateMotion.receive(on: RunLoop.main).sink(receiveValue: { [weak self] motion in
             guard let strongSelf = self else { return }
-            let acceleration = motion.userAcceleration.y
-            strongSelf.state.accelerationY = acceleration
-            strongSelf.state.speedY = SpeedCalculator.calculateSpeed(currentSpeed: strongSelf.state.speedY, acceleration: acceleration, delta: strongSelf.delta)
-            print(motion.userAcceleration.y)
+            let ax = motion.userAcceleration.x
+            let ay = motion.userAcceleration.y
+            let az = motion.userAcceleration.z
+            let roll = motion.attitude.roll
+            let pitch = motion.attitude.pitch
+            
+            let horizontalAcc = SpeedCalculator.calculateHorizontalComponent(x: ax, y: ay, z: az, roll: roll, pitch: pitch)
+            strongSelf.state.acc = SpeedCalculator.smooth(current: horizontalAcc, previous: strongSelf.smoothedHorizontalAcc)
+            strongSelf.state.speed = SpeedCalculator.calculateSpeed(currentSpeed: strongSelf.state.speed,
+                                                                    acc: strongSelf.state.acc,
+                                                                    delta: 1 / strongSelf.fps,
+                                                                    accThresh: strongSelf.accThresh)
+            // 一定秒数秒間acc≒0が続いたら停止しているとみなしてリセット
+            if strongSelf.state.acc < strongSelf.stoppingThresh {
+                strongSelf.stoppingCounter += 1
+            } else {
+                strongSelf.stoppingCounter = 0
+            }
+            if strongSelf.stoppingCounter > Int(strongSelf.fps * strongSelf.stoppingResetInterval) {
+                strongSelf.state.speed = 0
+                strongSelf.stoppingCounter = 0
+            }
         }).store(in: &cancellables)
     }
     
@@ -43,7 +70,11 @@ import Foundation
     }
     
     func onTapStop() {
-        state.speedY = 0
+        state.speed = 0
         sensor.stopAccelerometer()
+    }
+    
+    func onTapReset() {
+        state.speed = 0
     }
 }
